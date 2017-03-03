f5c934fd3a0e77bd8ac77c81085fabf1a82bc54b
zach beecher <zachbeecher@gmail.com>
2017-03-03 14:53:32 -0800

    add logic loops if else if etc, afraid it doesn't work that well, need to work it more. Also more style for user display

--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,71 @@
+
 function Pizza (pizzaSize, pizzaToppings, pizzaCrust) {
   this.pizzaSize = pizzaSize;
   this.pizzaToppings = pizzaToppings;
   this.pizzaCrust = pizzaCrust;
   this.cost;
 }
-
 Pizza.prototype.calculator = function() {
-  this.cost = 8;
+  this.cost = 25;
   if (this.size === 'personal') {
-    this.cost -= 4;
+    this.cost -= 5;
   } else if (this.size === 'small') {
     this.cost += 1;
   }
-  if (this.size === 'medium') {
-    this.cost += 3;
-  } else if (this.size === 'large') {
+  else if (this.size === 'medium') {
     this.cost += 5;
+  } else if (this.size === 'large') {
+    this.cost += 10;
   }
   if (this.pizzaCrust === 'white') {
     this.cost += 1;
   } else if (this.pizzaCrust === 'wheat') {
-    this.cost += 2;
+    this.cost += 3;
   }
-  if (this.pizzaCrust === 'sourdough') {
+  else if (this.pizzaCrust === 'sourdough') {
     this.cost += 1;
   } else if (this.pizzaCrust === 'glutenFree') {
     this.cost += 2;
   }
-  for (var i = 0; i < this.pizzaToppings.length; i++) {
+  if (this.pizzaToppings === 'Mushrooms') {
+    this.cost += 1;
+  } else if (this.pizzaToppings === 'Peppers') {
+    this.cost += 1;
+  } else if (this.pizzaToppings === 'Garlic') {
+    this.cost += 1;
+  } else if (this.pizzaToppings === 'Peperoni') {
+    this.cost += 1;
+  }else if (this.pizzaToppings === 'Anchovies') {
+    this.cost += 1;
+  } else if (this.pizzaToppings === 'Bacon') {
+    this.cost += 1;
+  }else if (this.pizzaToppings === 'Tomatoes') {
+    this.cost += 1;
+  } else if (this.pizzaToppings === 'Sausage') {
     this.cost += 1;
   }
-  console.log(this.cost);
 };
-
 Pizza.prototype.userDisplay = function() {
-  return "Fantastic choices! Here's your order:" + this.pizzaSize + " " + this.pizzaCrust + " pizza: $" + this.cost;
-  console.log(userDisplay);
+  return "Fantastic choices! Here's your order: " + this.pizzaSize + " " + this.pizzaCrust + " pizza total cost: $" + this.cost;
+}
+var displayIt = function() {
+  $('#pizzaShow h5').text(goodPizza.userDisplay());
+  $('#toppings').text(goodPizza.pizzaToppings.join(", "));
+  console.log(displayIt);
 }
 $(function(){
   $("#formInput").submit(function(){
     event.preventDefault();
-    var crust = $("#pizzaCrustType").val();
-    var size = $("#pizzaSize").val();
-    var topping = $('.topping input:checkbox:checked').val();
-
-    var goodPizza = new Pizza(size, topping, crust);
-    console.log(size);
-    console.log(topping);
-    console.log(crust);
+    var pizzaSize = $("#pizzaSize").val();
+    var pizzaToppings = $('input[name=toppings1]:checkbox:checked');
+    var pizzaCrust = $("#pizzaCrustType").val();
+    var goodPizza = new Pizza(pizzaSize, pizzaToppings, pizzaCrust);
+    goodPizza.calculator();
+    console.log(pizzaSize);
+    console.log(pizzaToppings);
+    console.log(pizzaCrust);
     console.log(goodPizza);
-
+    $('#pizzaShow').append('<li><span class="pizzaTime">' + goodPizza.userDisplay() + '</span></li>');
+    $("formInput").reset();
   });
 });
