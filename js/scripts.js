8db7fed3358a492475b9c8d7cfceb170c4510202
zach beecher <zachbeecher@gmail.com>
2017-03-03 11:12:44 -0800

    playing around with logic, creating a for loop to check the toppings, and spit them out to the user

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,40 @@
-function Pizza (pizzaSize, pizzaToppings) {
+function Pizza (pizzaSize, pizzaToppings, pizzaCrust) {
   this.pizzaSize = pizzaSize;
   this.pizzaToppings = pizzaToppings;
+  this.pizzaCrust = pizzaCrust;
 }
-Pizza.prototype =function (){
-  /* formula in here*/
+
+  var size = new Pizza (0,0,0);
+
+Pizza.prototype.toppings = function() {
+var toppingsOrder = "";
+
+for (var i = 0; i < this.pizzaToppings.length; i++) {
+ if (i === 0) {
+   toppingsOrder += this.pizzaToppings[i]
+ } else {
+   toppingsOrder += ("," + this.pizzaToppings[i])
+ };
+}
+ return toppingsOrder;
+ console.log(toppingsOrder);
+}
+Pizza.prototype.calculator = function (){
+  var totalCost = 0;
 }
 
 $(function(){
-  $("#").submit(function(){
-    /*grab user input*/
+  $("#formInput").submit(function(){
+    event.preventDefault();
+    var pizzaSizes = parseInt($("#pizzaSize").val());
+    var pizzaTopping = parseInt($(".topping input:checkbox:checked").val());
+    var pizzaCrusts = parseInt($("#pizzaCrustType").val());
+    console.log(pizzaSizes);
+    console.log(pizzaTopping);
+    console.log(pizzaCrusts);
+
+
+
+
   });
+});
