1a55e5efecb3da821c1dd2826a159790de7cf00d
zach beecher <zachbeecher@gmail.com>
2017-03-03 12:04:45 -0800

    add more logic to scripts, this will spit out size, topping, crust, and total cost to the user.

--- js/scripts.js
+++ js/scripts.js
@@ -2,39 +2,53 @@ function Pizza (pizzaSize, pizzaToppings, pizzaCrust) {
   this.pizzaSize = pizzaSize;
   this.pizzaToppings = pizzaToppings;
   this.pizzaCrust = pizzaCrust;
+  this.cost;
 }
 
-  var size = new Pizza (0,0,0);
+Pizza.prototype.calculator = function() {
+  this.cost = 8;
+  if (this.size === 'personal') {
+    this.cost -= 4;
+  } else if (this.size === 'small') {
+    this.cost += 1;
+  }
+  if (this.size === 'medium') {
+    this.cost += 3;
+  } else if (this.size === 'large') {
+    this.cost += 5;
+  }
+  if (this.pizzaCrust === 'white') {
+    this.cost += 1;
+  } else if (this.pizzaCrust === 'wheat') {
+    this.cost += 2;
+  }
+  if (this.pizzaCrust === 'sourdough') {
+    this.cost += 1;
+  } else if (this.pizzaCrust === 'glutenFree') {
+    this.cost += 2;
+  }
+  for (var i = 0; i < this.pizzaToppings.length; i++) {
+    this.cost += 1;
+  }
+  console.log(this.cost);
+};
 
-Pizza.prototype.toppings = function() {
-var toppingsOrder = "";
-
-for (var i = 0; i < this.pizzaToppings.length; i++) {
- if (i === 0) {
-   toppingsOrder += this.pizzaToppings[i]
- } else {
-   toppingsOrder += ("," + this.pizzaToppings[i])
- };
+Pizza.prototype.userDisplay = function() {
+  return "Fantastic choices! Here's your order:" + this.pizzaSize + " " + this.pizzaCrust + " pizza: $" + this.cost;
+  console.log(userDisplay);
 }
- return toppingsOrder;
- console.log(toppingsOrder);
-}
-Pizza.prototype.calculator = function (){
-  var totalCost = 0;
-}
-
 $(function(){
   $("#formInput").submit(function(){
     event.preventDefault();
-    var pizzaSizes = parseInt($("#pizzaSize").val());
-    var pizzaTopping = parseInt($(".topping input:checkbox:checked").val());
-    var pizzaCrusts = parseInt($("#pizzaCrustType").val());
-    console.log(pizzaSizes);
-    console.log(pizzaTopping);
-    console.log(pizzaCrusts);
+    var crust = $("#pizzaCrustType").val();
+    var size = $("#pizzaSize").val();
+    var topping = $('.topping input:checkbox:checked').val();
 
-
-
+    var goodPizza = new Pizza(size, topping, crust);
+    console.log(size);
+    console.log(topping);
+    console.log(crust);
+    console.log(goodPizza);
 
   });
 });
